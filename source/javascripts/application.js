5d5765b91aa8fabe446918b526b754cbae701d27
Dai Akatsuka <d.akatsuka@gmail.com>
2013-12-29 20:42:56 +0900

    テンプレートの修正

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,9 +1,1 @@
-$(".share .facebook a").on('click', function(e) {
-  e.preventDefault();
-  window.open(this.href, 'FBwindow', 'width=650, height=450, menubar=no, toolbar=no, scrollbars=yes');
-});
 
-$(".share .twitter a").on('click', function(e) {
-  e.preventDefault();
-  window.open(this.href, 'TWwindow', 'width=650, height=450, menubar=no, toolbar=no, scrollbars=yes');
-});
